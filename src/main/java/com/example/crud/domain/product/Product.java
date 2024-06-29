e9b90f373e422513192ac356e2ad4993959d3f33
lucaslimasz <id.lucas@outlook.com.br>
2024-06-29 02:18:44 -0300

    feat: get, post, conection db

--- src/main/java/com/example/crud/domain/product/Product.java
+++ src/main/java/com/example/crud/domain/product/Product.java
@@ -1,30 +1,73 @@
 package com.example.crud.domain.product;
 
 import jakarta.persistence.*;
-import lombok.*;
-
-import java.util.UUID;
+import java.util.Objects;
 
 @Table(name="product")
 @Entity(name="product")
-@Getter
-@Setter
-@AllArgsConstructor
-@NoArgsConstructor
-@EqualsAndHashCode(of = "id")
 public class Product {
-    @Id @GeneratedValue(strategy = GenerationType.UUID)
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.UUID)
     private String id;
 
     private String name;
 
     private Integer price_in_cents;
 
-    private Boolean active;
+    // Construtor padrão
+    public Product() {
+    }
 
     public Product(RequestProduct requestProduct){
         this.name = requestProduct.name();
         this.price_in_cents = requestProduct.price_in_cents();
-        this.active = true;
+    }
+
+    // Construtor com todos os parâmetros
+    public Product(String id, String name, Integer price_in_cents) {
+        this.id = id;
+        this.name = name;
+        this.price_in_cents = price_in_cents;
+    }
+
+    // Getters
+    public String getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public Integer getPrice_in_cents() {
+        return price_in_cents;
+    }
+
+    // Setters
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setPrice_in_cents(Integer price_in_cents) {
+        this.price_in_cents = price_in_cents;
+    }
+
+    // equals e hashCode usando o campo 'id'
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Product product = (Product) o;
+        return Objects.equals(id, product.id);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id);
     }
 }
--- src/main/java/com/example/crud/domain/product/ProductRepository.java
+++ src/main/java/com/example/crud/domain/product/ProductRepository.java
@@ -2,9 +2,4 @@ package com.example.crud.domain.product;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 
-import java.util.List;
-import java.util.UUID;
-
-public interface ProductRepository extends JpaRepository<Product, String> {
-    List<Product> findAllByActiveTrue();
-}
+public interface ProductRepository extends JpaRepository<Product, String> {}
